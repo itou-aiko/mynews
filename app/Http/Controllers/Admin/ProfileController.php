1a286282bd56cec21a76ffafb0111ab0eeb06a41
itou <aikodon0726@icloud.com>
2025-01-12 08:26:39 +0000

    ProfileController作成

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,5 +1,5 @@
 <?php
-
+//課題4:【応用】 artisanを使って、Admin/ProfileControllerを作成しましょう（→作成）
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //課題5.として以下を追記
+    //課題5:【応用】 Admin/ProfileControllerに、以下のadd, create, edit, update それぞれのActionを追加してみましょう
     public function add()
     {
         return view('admin.profile.create');
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,13 @@ use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add');
 });
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
+
+
 Auth::routes();
-
-use App\Http\Controllers\Admin\
-
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

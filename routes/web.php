09243881ad33fa3509b7c44e6acf5e15d268b2b8
itou <aikodon0726@icloud.com>
2025-01-28 16:55:33 +0000

    【PHP/Laravel】07 課題

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,16 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
-// 課題3:http://◯◯◯◯.jp/XXX というアクセスが来たときに、 
-// AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
-//(以下回答)
-
-// Route::controller(AAAController::class)->group(function() {
-//     Route::get('XXX', 'bbb');
-// });
-
-//課題4:【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
-//web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
-//admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
+//課題3【応用】現在はログインしてない状態で /admin/profile/edit にアクセスしたらProfileController の edit Action に割り当てるように設定されています。
+//こちらをログインしていない状態で /admin/profile/edit にアクセスした場合にログイン画面にリダイレクトされるように設定しましょう
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
 
 
